9ed83912f2f1cd907fe9092790e383819a174e2e
Sakari Ailus <sakari.ailus@linux.intel.com>
2021-04-22 15:59:48 +0300

    media: entity: Add an iterator helper for connected pads
    Add a helper macro for iterating over pads that are connected through
    enabled routes. This can be used to find all the connected pads within an
    entity, for instance starting from the pad which has been obtained during
    the graph walk.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Reviewed-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    
    - Make __media_entity_next_routed_pad() return NULL and adjust the
      iterator to handle that
    Signed-off-by: Jacopo Mondi <jacopo+renesas@jmondi.org>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

--- drivers/media/mc/mc-entity.c
+++ drivers/media/mc/mc-entity.c
@@ -274,6 +274,20 @@ static bool media_entity_has_route(struct media_entity *entity,
 	return entity->ops->has_route(entity, pad0, pad1);
 }
 
+struct media_pad *__media_entity_next_routed_pad(struct media_pad *root,
+						 struct media_pad *iter)
+{
+	struct media_entity *entity = root->entity;
+
+	for (; iter < &entity->pads[entity->num_pads]; iter++) {
+		if (media_entity_has_route(entity, root->index, iter->index))
+			return iter;
+	}
+
+	return NULL;
+}
+EXPORT_SYMBOL_GPL(__media_entity_next_routed_pad);
+
 /* push an entity to traversal stack */
 static void stack_push(struct media_graph *graph, struct media_pad *pad)
 {
